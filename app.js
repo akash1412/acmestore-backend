d5dce210cfe6a90bae71a3b791090b8e8a1a06d3
akash prasad <akashorasad2000@gmail.com>
2021-08-08 14:08:33 +0530

    bug fix 2

--- app.js
+++ app.js
@@ -13,11 +13,7 @@ config();
 
 const app = express();
 
-app.use(
-  cors({
-    origin: 'http://localhost:3000/',
-  })
-);
+app.use(cors());
 app.use(express.json());
 
 app.use('/api/v1/store', storeRouter);
